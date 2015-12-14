2003f074404112dea807362185292e38204b84e5
Anthony Granger <grangeranthony@gmail.com>
2015-12-14 23:21:58 +0100

    Finish mobile menu

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
+function toggleBurger() {
+	var el = document.getElementById('main-menu');
+	var elBurger = document.getElementById('burger-green');
+	var elOverlay = document.getElementById('overlay');
 
+	var cls = el.classList;
+
+	if (!cls.contains('toggled')) {
+		cls.add('toggled');
+		elOverlay.classList.add('active');
+		elBurger.classList.remove('green');
+	}
+	else {
+		cls.remove('toggled');
+		elOverlay.classList.remove('active');
+		elBurger.classList.add('green');
+	}
+
+	return false;
+}
